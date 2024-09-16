6e2944722c4b0bb4520a41e8cd23afa7055a4a1a
Francis Li <francisli@users.noreply.github.com>
2024-09-16 11:41:34 -0700

    [Closes #29] Date extenstion to calculate current age from date (#30)

--- PRKit/Classes/Extensions/DateExtension.swift
+++ PRKit/Classes/Extensions/DateExtension.swift
@@ -52,4 +52,66 @@ public extension Date {
             return other.timeIntervalSinceReferenceDate - timeIntervalSinceReferenceDate
         }
     }
+
+    /** A date generator function that can be overridden in tests */
+    static var nowFunc: (() -> Date)?
+    static var now: Date {
+        return Date.nowFunc?() ?? Date()
+    }
+
+    /** Adapted from: https://gist.github.com/andatta/3b2010604aa4bd553bf0098470f6dbb8 */
+    var age: (years: Int, months: Int, days: Int) {
+        var years = 0
+        var months = 0
+        var days = 0
+
+        let cal = Calendar.current
+        let now = Date.now
+        years = cal.component(.year, from: now) -  cal.component(.year, from: self)
+
+        let currMonth = cal.component(.month, from: now)
+        let birthMonth = cal.component(.month, from: self)
+
+        // get difference between current month and birthMonth
+        months = currMonth - birthMonth
+        // if month difference is in negative then reduce years by one and calculate the number of months.
+        if months < 0 {
+            years = years - 1
+            months = 12 - birthMonth + currMonth
+            if cal.component(.day, from: now) < cal.component(.day, from: self) {
+                months = months - 1
+            }
+        } else if months == 0 && cal.component(.day, from: now) < cal.component(.day, from: self) {
+            years = years - 1
+            months = 11
+        }
+
+        // calculate the days
+        if cal.component(.day, from: now) > cal.component(.day, from: self) {
+            days = cal.component(.day, from: now) - cal.component(.day, from: self)
+        } else if cal.component(.day, from: now) < cal.component(.day, from: self) {
+            let today = cal.component(.day, from: now)
+            let date = cal.date(byAdding: .month, value: -1, to: now)
+            days = date!.daysInMonth - cal.component(.day, from: self) + today
+        } else {
+            days = 0
+            if months == 12 {
+                years = years + 1
+                months = 0
+            }
+        }
+        return (years, months, days)
+    }
+
+    var daysInMonth: Int {
+        let calendar = Calendar.current
+
+        let dateComponents = DateComponents(year: calendar.component(.year, from: self), month: calendar.component(.month, from: self))
+        let date = calendar.date(from: dateComponents)!
+
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        let numDays = range.count
+
+        return numDays
+    }
 }
