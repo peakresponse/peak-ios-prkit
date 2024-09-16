74157f248d383a76bcc5338a0e2e7cf4c462d8e2
Francis Li <mail@francisli.com>
2024-09-16 11:21:09 -0700

    [Closes #29] Date extenstion to calculate current age from date

--- Example/Tests/DateExtensionTests.swift
+++ Example/Tests/DateExtensionTests.swift
@@ -12,10 +12,34 @@ class DateExtensionTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        Date.nowFunc = {
+            return ISO8601DateFormatter.date(from: "2024-09-16T18:14:39.613Z")!
+        }
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        Date.nowFunc = nil
+    }
+
+    func testAge() {
+        var date = ISO8601DateFormatter.date(from: "1977-12-26")
+        var (years, months, days) = date!.age
+        XCTAssertEqual(years, 46)
+        XCTAssertEqual(months, 8)
+        XCTAssertEqual(days, 21)
+
+        date = ISO8601DateFormatter.date(from: "2023-11-25")
+        (years, months, days) = date!.age
+        XCTAssertEqual(years, 0)
+        XCTAssertEqual(months, 9)
+        XCTAssertEqual(days, 22)
+
+        date = ISO8601DateFormatter.date(from: "2024-09-07")
+        (years, months, days) = date!.age
+        XCTAssertEqual(years, 0)
+        XCTAssertEqual(months, 0)
+        XCTAssertEqual(days, 9)
     }
 
     func testAsISO8601DateString() {
