6bc6fbede98ac3920f143f6c824fcd6fe12fb74f
Francis Li <mail@francisli.com>
2025-08-26 18:19:43 -0700

    Setting up a new view controller for custom components

---
+++ Example/PRKit/CustomViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomViewController.swift
+//  PRKit
+//
+//  Created by Francis Li on 8/26/25.
+//  Copyright © 2025 CocoaPods. All rights reserved.
+//
+
+import PRKit
+import UIKit
+
+final class CustomViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
