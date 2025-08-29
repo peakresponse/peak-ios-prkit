eb214270cee24eafb21cae8041ac12be77f3c688
Francis Li <mail@francisli.com>
2025-08-29 09:24:49 -0700

    First pass, TemperatureField

--- Example/PRKit/CustomViewController.swift
+++ Example/PRKit/CustomViewController.swift
@@ -9,10 +9,41 @@
 import PRKit
 import UIKit
 
-final class CustomViewController: UIViewController {
+final class CustomViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBackground
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 20)
+        ])
+
+        let inputAccessoryView = FormInputAccessoryView(rootView: view)
+
+        let tempField = TemperatureField()
+        tempField.labelText = "Temp."
+        tempField.translatesAutoresizingMaskIntoConstraints = false
+        tempField.tag = 1
+        tempField.inputAccessoryView = inputAccessoryView
+        stackView.addArrangedSubview(tempField)
     }
 }
---
+++ PRKit/Classes/Fields/TemperatureField.swift
@@ -0,0 +1,148 @@
+//
+//  TemperatureField.swift
+//  Pods
+//
+//  Created by Francis Li on 8/28/25.
+//
+
+import UIKit
+
+open class TemperatureField: FormComponent, FormComponentDelegate {
+    open var fField: TextField!
+    open var cField: TextField!
+
+    open override var source: NSObject? {
+        get { return cField.source }
+        set {
+            cField.source = newValue
+            fField.source = newValue
+        }
+    }
+    open override var target: NSObject? {
+        get { return cField.target }
+        set {
+            cField.target = newValue
+            fField.target = newValue
+        }
+    }
+
+    open override var attributeKey: String? {
+        get { return cField.attributeKey }
+        set {
+            cField.attributeKey = newValue
+            if let newValue = newValue {
+                fField.attributeKey = "\(newValue)F"
+            } else {
+                fField.attributeKey = nil
+            }
+        }
+    }
+
+    open override var inputAccessoryView: UIView? {
+        get { return fField.inputAccessoryView }
+        set {
+            fField.inputAccessoryView = newValue
+            cField.inputAccessoryView = newValue
+        }
+    }
+
+    open var labelText: String? {
+        didSet {
+            fField.labelText = "\(labelText ?? "") (°F)"
+            cField.labelText = "\(labelText ?? "") (°C)"
+        }
+    }
+
+    open override var tag: Int {
+        get { return 0 }
+        set {
+            fField.tag = newValue
+            cField.tag = newValue + 1
+        }
+    }
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    open func commonInit() {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            bottomAnchor.constraint(equalTo: stackView.bottomAnchor)
+        ])
+
+        let fField = TextField()
+        fField.attributeType = .decimal
+        fField.delegate = self
+        stackView.addArrangedSubview(fField)
+        self.fField = fField
+
+        let cField = TextField()
+        cField.attributeType = .decimal
+        cField.delegate = self
+        stackView.addArrangedSubview(cField)
+        self.cField = cField
+    }
+
+    open override func reloadInputViews() {
+        fField.reloadInputViews()
+        cField.reloadInputViews()
+    }
+
+    open override func didUpdateAttributeValue() {
+        cField.attributeValue = attributeValue
+        // check for a corresponding F value
+        let obj = source ?? target
+        fField.attributeValue = obj?.value(forKeyPath: "\(attributeKey ?? "")F") as? NSObject
+    }
+
+    open override func didUpdateEditing() {
+        fField.isEditing = isEditing
+        cField.isEditing = isEditing
+    }
+
+    open override func didUpdateEnabled() {
+        fField.isEnabled = isEnabled
+        cField.isEnabled = isEnabled
+    }
+
+    // MARK: - FormComponentDelegate
+
+    @objc open func formComponentDidChange(_ component: FormComponent) {
+        if component === fField {
+            // convert to Celsius
+            if let text = fField.text, let value = Double(text) {
+                let celsius = (value - 32) * 5 / 9
+                cField.attributeValue = String(format: "%.1f", celsius) as NSString
+            } else {
+                cField.attributeValue = nil
+            }
+            delegate?.formComponentDidChange?(cField)
+        } else if component === cField {
+            // convert to Fahrenheit
+            if let text = cField.text, let value = Double(text) {
+                let fahrenheit = (value * 9 / 5) + 32
+                fField.attributeValue = String(format: "%.1f", fahrenheit) as NSString
+            } else {
+                fField.attributeValue = nil
+            }
+            delegate?.formComponentDidChange?(fField)
+        }
+        delegate?.formComponentDidChange?(component)
+    }
+}
