ef2bfde0ebd8fc9ab4ef2a51f45ec1988883ca80
Francis Li <francisli@users.noreply.github.com>
2026-05-15 10:20:05 -0700

    [Closes #37] New TupleKeyboardSource implementation (#38)

---
+++ Sources/PRKit/Fields/Keyboards/TupleKeyboardSource.swift
@@ -0,0 +1,60 @@
+//
+//  TupleKeyboardSource.swift
+//  PRKit
+//
+//  Created by Francis Li on 5/15/26.
+//
+
+import Foundation
+
+open class TupleKeyboardSource: KeyboardSource {
+    public let name: String
+    public let items: [(String, String)]
+    public var filtered: [(String, String)]?
+
+    public init(name: String, items: [(String, String)]) {
+        self.name = name
+        self.items = items
+    }
+
+    public func count() -> Int {
+        if let filtered = filtered {
+            return filtered.count
+        }
+        return items.count
+    }
+
+    public func firstIndex(of value: NSObject) -> Int? {
+        if let filtered = filtered {
+            return filtered.firstIndex(where: { $0.1 == (value as? String) })
+        }
+        return items.firstIndex(where: { $0.1 == (value as? String) })
+    }
+
+    public func search(_ query: String?, callback: ((Bool) -> Void)?) {
+        if let query = query?.trimmingCharacters(in: .whitespacesAndNewlines), !query.isEmpty {
+            filtered = items.filter({ $0.0.localizedLowercase.contains(query.localizedLowercase) })
+        } else {
+            filtered = nil
+        }
+        callback?(false)
+    }
+
+    public func title(for value: NSObject?) -> String? {
+        return items.first(where: { $0.1 == (value as? String) })?.0
+    }
+
+    public func title(at index: Int) -> String? {
+        if let filtered = filtered {
+            return filtered[index].0
+        }
+        return items[index].0
+    }
+
+    public func value(at index: Int) -> NSObject? {
+        if let filtered = filtered {
+            return filtered[index].1 as NSObject
+        }
+        return items[index].1 as NSObject
+    }
+}
